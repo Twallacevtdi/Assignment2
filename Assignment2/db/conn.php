7d6c57d0be7c9ecdc589363dedf97f2635b02a84
Twallacevtdi <146570081+Twallacevtdi@users.noreply.github.com>
2023-11-11 13:38:31 -0500

    Add files via upload

---
+++ Assignment2/db/conn.php
@@ -0,0 +1,29 @@
+<?php 
+    $host = "localhost";
+    $db = "attendance_db";
+    $user = "root";
+    $pass = "";
+    $charset = "utf8mb4";
+
+
+
+   /* $host = "sql310.infinityfree.com";
+    $db = "if0_35096201_attendance_db";
+    $user = "f0_35096201";
+    $pass = " 59U3Z3aR0c94S ";
+    $charset = "utf8mb4";*/
+
+    $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+
+    try{
+        $pdo = new PDO($dsn, $user, $pass);
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    } catch(PDOException $e) {
+        throw new PDOException($e->getMessage());
+    }
+
+    require_once "crud.php";
+    $crud = new crud($pdo);
+   
+?>
